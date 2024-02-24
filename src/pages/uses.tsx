c9c17425c3fe79333704149d2c7e8356bb0f7336
Rajesh Royal <rajeshroyal896@gmail.com>
2024-02-24 12:35:33 +0530

    Update uses.tsx

--- src/pages/uses.tsx
+++ src/pages/uses.tsx
@@ -18,7 +18,7 @@ const UsesPage: NextPage<IProps> = ({ markdown }) => {
 		<AnimatePage>
 			<SeoHead
 				title="Rajesh Royal uses ..."
-				description="This is a comprehensive list of tech equipment and software I use for my day-to-day work as a software engineer in the UK."
+				description="This is a comprehensive list of tech equipment and software I use for my day-to-day work as a software engineer in India."
 			/>
 			<Container>
 				<h1 className="headline text-3xl md:text-5xl lg:text-6xl mt-8">Uses</h1>
